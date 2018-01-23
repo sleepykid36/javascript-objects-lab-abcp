0a85bccede9d62ff5b5e2ad3227aed041b17f7ac
Shaun Tung <shaun.tung@gmail.com>
2018-01-23 02:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, obj, { [key]: value })
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
- 
-  return obj
-}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return
+  
